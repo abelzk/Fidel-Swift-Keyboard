be1569e0c46698dfab6923d157f2c7ba46b64bc0
Abel Zecharias <89125605+abelzk@users.noreply.github.com>
2025-06-18 16:16:57 +0300

    Enhance navbar functionality: add close icon and improve toggle behavior with visual feedback

--- scripts/navToggle.js
+++ scripts/navToggle.js
@@ -1,12 +1,35 @@
+// function navToggle() {
+//   const navLinks = document.getElementById("navbar-links");
+//   const overlay = document.getElementById("navbar-overlay");
+
+//   const isClosed = navLinks.getAttribute("data-state") === "closed";
+//   const newState = isClosed ? "open" : "closed";
+
+//   navLinks.setAttribute("data-state", newState);
+//   overlay.setAttribute("data-state", newState);
+// }
+// document.getElementById("navbar-overlay").addEventListener("click", () => {
+//   const navLinks = document.getElementById("navbar-links");
+//   const overlay = document.getElementById("navbar-overlay");
+
+//   navLinks.setAttribute("data-state", "closed");
+//   overlay.setAttribute("data-state", "closed");
+// });
+
 function navToggle() {
   const navLinks = document.getElementById("navbar-links");
   const overlay = document.getElementById("navbar-overlay");
+  const menuIcon = document.getElementById("menuIcon");
 
   const isClosed = navLinks.getAttribute("data-state") === "closed";
   const newState = isClosed ? "open" : "closed";
 
   navLinks.setAttribute("data-state", newState);
   overlay.setAttribute("data-state", newState);
+
+  // Change the icon
+  menuIcon.src = isClosed ? "images/close.svg" : "images/menu.svg";
+  menuIcon.alt = isClosed ? "close icon" : "menu icon";
 }
 document.getElementById("navbar-overlay").addEventListener("click", () => {
   const navLinks = document.getElementById("navbar-links");
