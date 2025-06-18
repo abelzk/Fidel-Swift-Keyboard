74bf5fd6b627b7849c64e3a375ebbdc4cbd6dffd
Abel Zecharias <89125605+abelzk@users.noreply.github.com>
2025-06-18 16:21:23 +0300

    Enhance navbar functionality: update menu icon on overlay click for better user experience

--- scripts/navToggle.js
+++ scripts/navToggle.js
@@ -34,7 +34,12 @@ function navToggle() {
 document.getElementById("navbar-overlay").addEventListener("click", () => {
   const navLinks = document.getElementById("navbar-links");
   const overlay = document.getElementById("navbar-overlay");
+    const isClosed = navLinks.getAttribute("data-state") === "closed";
+  const newState = isClosed ? "open" : "closed";
 
   navLinks.setAttribute("data-state", "closed");
   overlay.setAttribute("data-state", "closed");
+
+    menuIcon.src = isClosed ? "images/close.svg" : "images/menu.svg";
+  menuIcon.alt = isClosed ? "close icon" : "menu icon";
 });
