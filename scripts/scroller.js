c822b84f7d17ed212ca2f4480e1c79c86a2ea5ca
Abel Zecharias <89125605+abelzk@users.noreply.github.com>
2025-06-19 22:40:52 +0300

    Add brand images for Notion, OneNote, Telegram, and Zoho Books; implement scrolling animation
    - Added PNG images for Notion, OneNote, Telegram, and Zoho Books to the brands directory.
    - Implemented a scrolling animation in scroller.js that duplicates content for a seamless scrolling effect, respecting user preferences for reduced motion.

---
+++ scripts/scroller.js
@@ -0,0 +1,21 @@
+const scrollers = document.querySelectorAll('.scroller')
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true)
+
+        const scrollerInner = scroller.querySelector('.scroller-inner')
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicateItem = item.cloneNode(true);
+            duplicateItem.setAttribute("area-hidden", true);
+            scrollerInner.appendChild(duplicateItem);
+            
+        }) 
+
+    });
+}
